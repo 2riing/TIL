269df9a154ca873daaeda7c86343afda121688f5
2riing <2riing2@gmail.com>
2022-01-19 16:27:55 +0900

    save

---
+++ Python/a.py
@@ -0,0 +1,9 @@
+values = list()
+
+values[
+	{'name' = ['염수홍', '조용준', '이우현']
+	 'age' = '29'	
+]
+
+
+values[0]['name'][0]
---
+++ Python/python2.py
@@ -0,0 +1,9 @@
+# 숫자를 받아서- input
+# 세제곱 결과를 반환 - output
+# 호출 : cube(2), cube(10) ...
+def cube(number):
+    return number ** 3
+    # 세제곱 해주는 함수 
+print(cube(2))
+
+
