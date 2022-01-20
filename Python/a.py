c10757d82ac03f4d5fe32e2378ce0d0baec654c7
2riing <2riing2@gmail.com>
2022-01-20 21:23:40 +0900

    add

--- Python/a.py
+++ Python/a.py
@@ -1,9 +1,14 @@
-values = list()
+# values = list()
 
-values[
-	{'name' = ['염수홍', '조용준', '이우현']
-	 'age' = '29'	
-]
+# values[
+# 	{'name' = ['염수홍', '조용준', '이우현']
+# 	 'age' = '29'	
+# ]
 
 
-values[0]['name'][0]
+# values[0]['name'][0]
+
+def add(a, **args):
+	return a, args
+
+print(add(2, a=100, b=2))
--- Python/python2.py
+++ Python/python2.py
@@ -7,3 +7,18 @@ def cube(number):
 print(cube(2))
 
 
+# values = list()
+
+# values[
+# 	{'name' = ['염수홍', '조용준', '이우현']
+# 	 'age' = '29'	
+# ]
+
+
+# values[0]['name'][0]
+
+def add(a, **args):
+	return a, args
+
+print(add(2, b=2))
+# print(add(2, a=100, b=2)) 는 에러 
