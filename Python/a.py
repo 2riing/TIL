d4d8e425eaaa06d2bb51f24827e01e901b9aa488
2riing <2riing2@gmail.com>
2022-01-24 13:21:31 +0900

    11

--- Python/a.py
+++ Python/a.py
@@ -1,12 +1,21 @@
 
-
-a = '3.99'
-b = float(a)
-print(b)
-print(int(b))
-
-print(int(3.5))
-print(int('3.5'))
+from random import sample
 
 
+sample_dict = {'sample_key1': 'first_value' , 'sample_key2' : 'second_value',}
+
+
+# sample_dict['sample_key3'] = 'third_value'
+
+# for i,k in sample_dict.get['sample_key1']:
+#     print(i,k)
+
+
+
+
+# print(sample_dict)
+# print(sample_dict['sample_key1'])
+sample_dict.pop('sample_key1')
+
+print(sample_dict)
 
